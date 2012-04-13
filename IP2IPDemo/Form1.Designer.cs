cf8c1a1ffdd794bf2ad34283882330b32048ac22
GSiry <support@avspeed.com>
2012-04-13 10:55:34 -0400

    Initial Check in

---
+++ IP2IPDemo/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace IP2IPDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnCall = new System.Windows.Forms.Button();
+            this.txtIP = new System.Windows.Forms.TextBox();
+            this.btnHangUp = new System.Windows.Forms.Button();
+            this.icc = new iConfClient.NET.iConfClientDotNet();
+            this.ics = new iConfServer.NET.iConfServerDotNet();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCall
+            // 
+            this.btnCall.Location = new System.Drawing.Point(432, 340);
+            this.btnCall.Name = "btnCall";
+            this.btnCall.Size = new System.Drawing.Size(50, 23);
+            this.btnCall.TabIndex = 6;
+            this.btnCall.Text = "Call";
+            this.btnCall.UseVisualStyleBackColor = true;
+            this.btnCall.Click += new System.EventHandler(this.btnCall_Click);
+            // 
+            // txtIP
+            // 
+            this.txtIP.Location = new System.Drawing.Point(288, 343);
+            this.txtIP.Name = "txtIP";
+            this.txtIP.Size = new System.Drawing.Size(138, 20);
+            this.txtIP.TabIndex = 5;
+            // 
+            // btnHangUp
+            // 
+            this.btnHangUp.Location = new System.Drawing.Point(488, 340);
+            this.btnHangUp.Name = "btnHangUp";
+            this.btnHangUp.Size = new System.Drawing.Size(57, 23);
+            this.btnHangUp.TabIndex = 8;
+            this.btnHangUp.Text = "Hang up";
+            this.btnHangUp.UseVisualStyleBackColor = true;
+            this.btnHangUp.Click += new System.EventHandler(this.btnHangUp_Click);
+            // 
+            // icc
+            // 
+            this.icc.AutoReconnect = false;
+            this.icc.BackColor = System.Drawing.Color.Gray;
+            this.icc.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.icc.CallbackObject = null;
+            this.icc.Location = new System.Drawing.Point(288, 12);
+            this.icc.Name = "icc";
+            this.icc.Size = new System.Drawing.Size(398, 313);
+            this.icc.TabIndex = 4;
+            // 
+            // ics
+            // 
+            this.ics.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.ics.CallbackObject = null;
+            this.ics.Location = new System.Drawing.Point(12, 12);
+            this.ics.Name = "ics";
+            this.ics.Size = new System.Drawing.Size(214, 207);
+            this.ics.TabIndex = 7;
+            this.ics.VideoRateControl = iConfServer.NET.iConfServerDotNet.RateControl.ConstantBitRate;
+            this.ics.VideoPreviewStarted += new iConfServer.NET.iConfServerDotNet.VideoPreviewStartedDelegate(this.ics_VideoPreviewStarted);
+            this.ics.ClientDisconnected += new iConfServer.NET.iConfServerDotNet.ClientDisconnectedDelegate(this.ics_ClientDisconnected);
+            this.ics.IncomingCall += new iConfServer.NET.iConfServerDotNet.IncomingCallDelegate(this.ics_IncomingCall);
+            this.ics.CannotRunGraph += new iConfServer.NET.iConfServerDotNet.CannotRunGraphDelegate(this.ics_CannotRunGraph);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(175, 233);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "My Video";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(612, 328);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Remote Video";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(694, 387);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnHangUp);
+            this.Controls.Add(this.ics);
+            this.Controls.Add(this.btnCall);
+            this.Controls.Add(this.txtIP);
+            this.Controls.Add(this.icc);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Simple IP 2 IP Demo";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        internal System.Windows.Forms.Button btnCall;
+        internal System.Windows.Forms.TextBox txtIP;
+        internal iConfClient.NET.iConfClientDotNet icc;
+        private iConfServer.NET.iConfServerDotNet ics;
+        private System.Windows.Forms.Button btnHangUp;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ IP2IPDemo/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Collections;
+using iConfServer.NET.HelperClasses;
+
+namespace IP2IPDemo
+{
+    public partial class Form1 : Form
+    {
+        
+        /// <summary>
+        /// THIS APPLICATION NEEDS RUN THIS APPLICATION NEEDS TO RUN ON 2 DIFFERENT PCS
+        /// (OR Virtual Machines)
+        /// OR CAN BE USED IN CALLBACK mode by using the local ip when placing a call
+        /// This is a simple demo which uses basic settings
+        /// For help email us at support@avspeed.com
+        /// </summary>
+        public Form1()
+        {
+            InitializeComponent();
+            txtIP.Text = ics.GetLocalIp()[0].ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                ics.InitializeAudioSystem(iConfServer.NET.iConfServerDotNet.audioType.DirectSound, -1, -1, 16000, 10);
+               
+                //select the first available video device
+                ics.SelectVideoDevice(0);
+
+               
+                ArrayList lst = ics.GetVideoSizes();
+                
+                //start previewing video using the default video size 
+                ics.StartPreview(13);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(string.Format("Error initializing {0}", ex.Message));
+            }
+        }
+
+        
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            //stop listening
+            ics.Listen(false, ics.GetLocalIp()[0].ToString(), 9990, 17860, 17861);
+            
+            //stop the video preview
+            ics.StopPreview();
+            
+        }
+
+        private void btnCall_Click(object sender, EventArgs e)
+        {
+            string myIp = ics.GetLocalIp()[0].ToString();
+            string ipToCall = txtIP.Text;
+
+            const int videoPort = 9990;
+            const int audioTcpPort = 17860;
+            const int audioUdpPort = 17861;
+
+            //place a call to an iConf Server
+            //note when using the Call function we have the ability to supply callback parameters 
+            //which will help the peer connection call us back
+            icc.AutoReconnect = true;
+            icc.Call(ipToCall, videoPort, 0, 0, "test", ics.CallBackId, myIp, videoPort, audioTcpPort, audioUdpPort, "");
+        }
+
+
+        private void ics_IncomingCall(object sender, string authenticationData, int socketHandle, string callbackid, string callbackipaddress, int callbackvideoport, int callbackaudiotcpport, int callbackaudiudpport)
+        {
+            Invoke(new MethodInvoker(delegate
+            {
+                //accept the incoming call
+                ics.AcceptCall("n/a", socketHandle);
+
+                //call back to have a 1 on one video conference
+                icc.Call(callbackipaddress, callbackvideoport, 0, 0, "n/a", callbackid,
+                         ics.GetLocalIp()[0].ToString(), 0, 0, 0, "");
+            }));
+     
+        }
+
+        private void ics_ClientDisconnected(object sender, string ipAddress, int port)
+        {
+            Invoke(new MethodInvoker(delegate
+            {
+                //peer has disconnected
+                //disconnect our client as well
+                icc.Disconnect();
+            }));
+        }
+
+        private void ics_CannotRunGraph()
+        {
+            MessageBox.Show("Video Capture device might be in use");
+        }
+
+
+        private void btnHangUp_Click(object sender, EventArgs e)
+        {
+            icc.Disconnect();
+            icc.ClearImage();
+        }
+
+
+
+        private void ics_VideoPreviewStarted(int videoWidth, int videoHeight)
+        {
+            // thid is where we are going to initialize the video encoder -
+            // note that this is a change from previous versions
+
+            Invoke(new MethodInvoker(delegate
+            {
+                this.Text = string.Format("Simple IP 2 IP Demo - {0}x{1}", videoWidth, videoHeight);
+
+                ics.VideoRateControl = iConfServer.NET.iConfServerDotNet.RateControl.ConstantQuality;
+                //Initialize Codec
+                ics.SetEncoderProperties(VideoCodecs.MPEG4, 20, 8000, 0, 0, 0);
+
+                //listen for incoming connections
+                ics.Listen(true, ics.GetLocalIp()[0].ToString(), 9990, 17860, 17861);
+            }));
+            
+
+
+        }
+
+       
+
+        
+    }
+}
